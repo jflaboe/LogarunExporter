8bdcb3c0969885290e0b06fa25b4135f51da6430
fendull <john@laboe.com>
2022-01-20 02:08:34 -0800

    dependencies and shortening example date ranges

--- async_example.py
+++ async_example.py
@@ -2,7 +2,7 @@ from logarun import *
 import time
 
 api = API(username="jflaboe")
-resp = api.get_date_range("05/19/2015", "07/19/2016", wait_for_completion=False)
+resp = api.get_date_range("05/19/2015", "12/19/2015", wait_for_completion=False)
 
 while resp.is_complete() is False:
     print(resp.get_remaining_pct())
--- basic_example.py
+++ basic_example.py
@@ -1,5 +1,5 @@
 from logarun import *
 
 api = API(username="jflaboe")
-resp = api.get_date_range("05/19/2015", "07/19/2016", wait_for_completion=True)
+resp = api.get_date_range("05/19/2015", "12/19/2015", wait_for_completion=True)
 print(resp.get_response())
